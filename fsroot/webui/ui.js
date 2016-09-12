018aab0c054e85db25b81296bbefd7b647bf19aa
mark <markxr@gmail.com>
2016-09-12 16:24:56 +0100

    Javascript code to do PUT upload

--- fsroot/webui/ui.js
+++ fsroot/webui/ui.js
@@ -1,1 +1,33 @@
 // UI Javascript code. Loaded with defer.
+
+function add_controls()
+{
+    /*
+     * Add controls to page.
+     */
+    var d = document.createElement("div");
+    d.setAttribute("id", "controls");
+    d.innerHTML = 'ESP-8266 (esp-webui) \
+        UPLOAD: <input type="file" id="upf">\
+        more todo\
+        ';
+    document.documentElement.appendChild(d);
+    var uploader = document.getElementById("upf");
+    uploader.addEventListener("change", trigger_upload);
+}
+
+function trigger_upload()
+{
+    var file = this.files[0];
+    if (file) {
+        var xhr = new XMLHttpRequest();
+        var target_url= new URL(window.location);
+        target_url.href += file.name;
+        console.log(target_url.toString());
+        xhr.open("PUT", target_url.toString());
+        xhr.send(file);
+    }
+    
+}
+
+add_controls();
